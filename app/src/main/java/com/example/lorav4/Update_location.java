5e981277ab628e4f3b897bccf3cc55ef568d905c
EpicPrivacy <47374573+EpicPrivacy@users.noreply.github.com>
2023-11-06 16:24:23 +0800

    update

--- app/src/main/java/com/example/lorav4/Update_location.java
+++ app/src/main/java/com/example/lorav4/Update_location.java
@@ -1,14 +1,30 @@
 package com.example.lorav4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Update_location extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
+public class Update_location extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_location);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+
+        if(mapFragment != null){
+            mapFragment.getMapAsync(this);
+        }
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap){
+        map = googleMap;
     }
 }
